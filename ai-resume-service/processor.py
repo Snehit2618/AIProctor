62cd9f596796d1c27efbc9abacf77f61c4822c9c
Snehit2618 <snehitj17@gmail.com>
2026-03-24 01:32:05 +0530

    backend integrations for resume analysing done

--- ai-resume-service/processor.py
+++ ai-resume-service/processor.py
@@ -11,6 +11,7 @@ from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
 load_dotenv()
+
 # Load resources once at startup
 nlp = spacy.load("en_core_web_sm")
 model = joblib.load("utils/model.pkl")
